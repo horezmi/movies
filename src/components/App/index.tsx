d4227ec68868f910932f87f4fc399eacd28bd938
Aykhan <aykhan.abdullazade@gmail.com>
2021-05-16 22:32:31 +0300

    add antd, testing

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Card, Tabs, Pagination, Spin } from 'antd';
 
+import 'antd/dist/antd.css';
 import './index.scss';
 
-const App = () => (
-  <div className="App">
-    start
-  </div>
-);
+const App = () => {
+  const { Meta } = Card;
+
+  return (
+    <div className="App">
+      <div className="film-card">
+        <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
+      </div>
+      <Pagination />
+      <Tabs>
+        <div className="let">
+          <p>dqwdwqd</p>
+        </div>
+      </Tabs>
+      <Spin />
+    </div>
+  );
+};
 
 export default App;
