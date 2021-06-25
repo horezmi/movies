87f299b28b99bc318e140e4448557fa6a7a8b427
Aykhan <aykhan.abdullazade@gmail.com>
2021-06-25 13:47:25 +0300

    add constants

--- src/components/SearchTab/index.tsx
+++ src/components/SearchTab/index.tsx
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { SearchPanel, CardList, Pagination, Loader, Error } from 'components';
 import { getSearchedMovies, postRatedFilm } from 'api';
 import { MoviesType } from 'types/interfaces';
+import { DEFAULT_SEARCH_PARAM } from 'constants/index';
 
 import 'antd/dist/antd.css';
 import './index.scss';
-
-const DEFAULT_SEARCH = 'return';
 
 const SearchTab: React.FC = (): JSX.Element => {
   const [movies, setMovies] = useState<MoviesType[]>([]);
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [loading, setLoading] = useState<boolean>(true);
-  const [search, setSearch] = useState<string>(DEFAULT_SEARCH);
+  const [search, setSearch] = useState<string>(DEFAULT_SEARCH_PARAM);
   const [error, setError] = useState<boolean>(false);
 
   const getMoviesData = async () => {
@@ -37,7 +36,7 @@ const SearchTab: React.FC = (): JSX.Element => {
     setPage(pageNumber);
   };
   const onSearch = (value: string) => {
-    if (value === '') value = DEFAULT_SEARCH;
+    if (value === '') value = DEFAULT_SEARCH_PARAM;
     setLoading(true);
     setSearch(value);
   };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,5 @@ export const API_KEY: string = 'c44b5b2b9ec7f5830a7641106c455833';
 
 export const IMAGE_URL: string = 'https://image.tmdb.org/t/p/w500';
 export const DEFAULT_IMAGE_URL: string = 'https://www.wildhareboca.com/wp-content/uploads/sites/310/2018/03/image-not-available.jpg';
+
+export const DEFAULT_SEARCH_PARAM: string = 'internet';
